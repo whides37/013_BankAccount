c297761beb12985296b0accab55d231a5082af62
whides37 <w.hides37@gmail.com>
2026-02-13 14:36:41 +0900

    メソッド完成

--- BankAccount.py
+++ BankAccount.py
@@ -1,6 +1,11 @@
 class BankAccount:
 
-def __init__(self,name):
-    self.name = name
-    self.balance = 0
+    def __init__(self,name):
+       self.name = name
+       self.balance = 0
     
+    def deposit(self,amount):
+        self.balance += amount
+
+    def get_balance(self):
+        return self.balance
