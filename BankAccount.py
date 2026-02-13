2696f522e144d73b6573143a84d5395449bbe304
whides37 <w.hides37@gmail.com>
2026-02-13 15:29:21 +0900

    出金メソッドを追加

--- BankAccount.py
+++ BankAccount.py
@@ -9,3 +9,10 @@ class BankAccount:
 
     def get_balance(self):
         return self.balance
+    
+    def withdraw(self, amount):
+        if amount <= 0:
+           raise ValueError("出金額は1円以上である必要があります")
+        if amount > self.balance:
+           raise ValueError("残高不足です")
+        self.balance -= amount
