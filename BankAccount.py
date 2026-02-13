32bc8217ae2b57a7966cb479d9d358e64edf1e4d
whides37 <w.hides37@gmail.com>
2026-02-13 15:44:15 +0900

    振込メソッドと仮アカウントと仮入金振り込みコードを追加

--- BankAccount.py
+++ BankAccount.py
@@ -16,3 +16,27 @@ class BankAccount:
         if amount > self.balance:
            raise ValueError("残高不足です")
         self.balance -= amount
+
+    def transfer(self, other_account, amount):
+        if amount <= 0:
+            raise ValueError("振込額は1円以上である必要があります")
+
+        if amount > self.balance:
+            raise ValueError("残高不足です")
+
+        if not isinstance(other_account, BankAccount):
+            raise ValueError("振込先が不正です")
+
+        # 出金
+        self.balance -= amount
+        # 入金
+        other_account.balance += amount
+
+a = BankAccount("Kiki")
+b = BankAccount("Lala")
+
+a.deposit(1000)
+a.transfer(b, 300)
+
+print(a.get_balance())  # 700
+print(b.get_balance())  # 300
